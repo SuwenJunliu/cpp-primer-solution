53eca89950d3141fa491a43cd101c9debf8df12a
Rjerk <460139918@qq.com>
2017-04-02 01:27:13 +0800

    header

---
+++ head.h
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <cstddef>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <iomanip>
+#include <iterator>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <ctype.h>
+#include <utility>
+#include <initializer_list>
+#include <exception>
+#include <memory> //using shared_ptr, unique_ptr, weak_ptr, allocator
+#include <stdexcept> //using out_of_range
+#include <functional>
+#include <new> // bad_alloc, nothrow
+
+using namespace std::placeholders;
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+using std::istringstream;
+using std::ostringstream;
+using std::stringstream;
+using std::istream;
+using std::ostream;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::istream_iterator;
+using std::ostream_iterator;
+
+using std::vector;
+using std::list;
+using std::forward_list;
+using std::deque;
+using std::queue;
+using std::string;
+using std::pair;
+using std::stack;
+using std::map;
+using std::set;
+using std::multimap;
+using std::multiset;
+using std::unordered_set;
+using std::unordered_multiset;
+using std::unordered_map;
+using std::unordered_multimap;
+
+using std::runtime_error;
+using std::shared_ptr;
+using std::weak_ptr;
+using std::unique_ptr;
+using std::make_shared;
+using std::nothrow;
+using std::allocator;
+using std::out_of_range;
+using std::begin;
+using std::end;
+using std::initializer_list;
+using std::distance;
